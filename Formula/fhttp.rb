e8b9f8676a78a0c026e335f28f026e37e4754008
René Perschon <rene.perschon@akelius.de>
2020-07-24 13:16:45 +0200

    add MIT license

--- Formula/fhttp.rb
+++ Formula/fhttp.rb
@@ -4,6 +4,7 @@
 class Fhttp < Formula
     desc "File-based command line http client."
     homepage "https://github.com/Leopard2A5/fhttp"
+    license "MIT"
     url "https://github.com/Leopard2A5/fhttp/releases/download/1.3.0/fhttp.macosx.tar.gz"
     sha256 "28836b7ffac24f400eab5fa1877ba251ef583dbc436190b12e9b8249f8ae8736"
   
