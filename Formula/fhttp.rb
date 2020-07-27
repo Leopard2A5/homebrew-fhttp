7bf5af86db65b062d17d1ef772253970711e4efa
René Perschon <rene.perschon@akelius.de>
2020-07-27 13:53:52 +0200

    add script to generate formula

--- Formula/fhttp.rb
+++ Formula/fhttp.rb
@@ -1,14 +1,11 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Fhttp < Formula
-    desc "File-based command line http client."
-    homepage "https://github.com/Leopard2A5/fhttp"
-    license "MIT"
-    url "https://github.com/Leopard2A5/fhttp/releases/download/1.3.0/fhttp.macosx.tar.gz"
-    sha256 "28836b7ffac24f400eab5fa1877ba251ef583dbc436190b12e9b8249f8ae8736"
-  
-    def install
-      bin.install "fhttp"
-    end
+  desc 'File-based command line http client.'
+  homepage 'https://github.com/Leopard2A5/fhttp'
+  license 'MIT'
+  url 'https://github.com/Leopard2A5/fhttp/releases/download/1.3.0/fhttp.macosx.tar.gz'
+  sha256 '28836b7ffac24f400eab5fa1877ba251ef583dbc436190b12e9b8249f8ae8736'
+
+  def install
+    bin.install 'fhttp'
   end
+end
---
+++ Formula/write_formula.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+VERSION=${1:?version must be given}
+SHA=${2:?sha must be given}
+
+echo "class Fhttp < Formula
+  desc 'File-based command line http client.'
+  homepage 'https://github.com/Leopard2A5/fhttp'
+  license 'MIT'
+  url 'https://github.com/Leopard2A5/fhttp/releases/download/${VERSION}/fhttp.macosx.tar.gz'
+  sha256 '${SHA}'
+
+  def install
+    bin.install 'fhttp'
+  end
+end"
