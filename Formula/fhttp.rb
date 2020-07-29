2439aede24ee3b068ef91ecfe0b20f1e637db503
Add & Commit Action <actions@github.com>
2020-07-29 15:44:47 +0000

    release refs/tags/1.3.1

--- Formula/fhttp.rb
+++ Formula/fhttp.rb
@@ -2,8 +2,8 @@ class Fhttp < Formula
   desc 'File-based command line http client.'
   homepage 'https://github.com/Leopard2A5/fhttp'
   license 'MIT'
-  url 'https://github.com/Leopard2A5/fhttp/releases/download/1.3.0/fhttp.macosx.tar.gz'
-  sha256 '28836b7ffac24f400eab5fa1877ba251ef583dbc436190b12e9b8249f8ae8736'
+  url 'https://github.com/Leopard2A5/fhttp/releases/download/1.3.1/fhttp.macosx.tar.gz'
+  sha256 '70c4740d7db6aac7d23c32823a642ab5b511df9787b615e12dcabef215aa751f'
 
   def install
     bin.install 'fhttp'
