86f466b009f23119fb22b1a0066878653113e474
René Perschon <rperschon85@gmail.com>
2020-12-28 18:42:08 +0000

    release refs/tags/1.3.2

--- Formula/fhttp.rb
+++ Formula/fhttp.rb
@@ -3,7 +3,7 @@ class Fhttp < Formula
   homepage 'https://github.com/Leopard2A5/fhttp'
   license 'MIT'
   url 'https://github.com/Leopard2A5/fhttp/releases/download/1.3.2/fhttp.macosx.tar.gz'
-  sha256 'dac54a5641821fbbb2cce9b35d4cbdc60953939fab4ec3fbb1327959ff9b52ca'
+  sha256 'fd428660a8cdc9f331505e83d9630c0095cc4d8ee33e801357cbfc1c5cf3b69e'
 
   def install
     bin.install 'fhttp'
