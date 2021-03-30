e7be11cf9a5e5a4cc978e1f3d439c28247c0b3ba
René Perschon <rene.perschon@akelius.de>
2021-03-30 14:25:56 +0200

    Revert "release refs/tags/test"
    This reverts commit 64c23532f22eb5142c7e9e162d7427fce4389d79.

--- Formula/fhttp.rb
+++ Formula/fhttp.rb
@@ -2,8 +2,8 @@ class Fhttp < Formula
   desc 'File-based command line http client.'
   homepage 'https://github.com/Leopard2A5/fhttp'
   license 'MIT'
-  url 'https://github.com/Leopard2A5/fhttp/releases/download/test/fhttp.macosx.tar.gz'
-  sha256 '51b3c811944b06c54af76e1563cc93e5e9a17cc0580eb092ad57cf8920745e19'
+  url 'https://github.com/Leopard2A5/fhttp/releases/download/1.3.2/fhttp.macosx.tar.gz'
+  sha256 '15a00fbb282beb2e26487b2a537ca96e9ff61dcc4af0ff77867ebda37a3f3f08'
 
   def install
     bin.install 'fhttp'
