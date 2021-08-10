060f724a9cd2ef8540ac3348c053726a090f7313
René Perschon <rperschon85@gmail.com>
2021-08-10 19:41:24 +0200

    1.5.1

--- Formula/fhttp.rb
+++ Formula/fhttp.rb
@@ -2,8 +2,8 @@ class Fhttp < Formula
   desc 'File-based command line http client.'
   homepage 'https://github.com/Leopard2A5/fhttp'
   license 'MIT'
-  url 'https://github.com/Leopard2A5/fhttp/releases/download/1.5.0/fhttp.macosx.tar.gz'
-  sha256 '841ea50eb127f6c1eaf04a3d6486ab0db34b80d09bcb5a09f1b576e8aade765f'
+  url 'https://github.com/Leopard2A5/fhttp/releases/download/1.5.1/fhttp.macosx.tar.gz'
+  sha256 'f4e08db0161114c3846b6d44a744796a97eaf423211a65d7fd6da3b9059d23a5'
 
   def install
     bin.install 'fhttp'
